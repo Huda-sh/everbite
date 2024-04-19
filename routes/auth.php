bc600ee24d5cb158fe7974b680be250dad4d88f9
Huda Shakir <huda.f.shakir@gmail.com>
2024-04-19 13:08:50 +0300

    refactor: cleaned the web route file

---
+++ routes/auth.php
@@ -0,0 +1,29 @@
+<?php
+
+use App\Actions\Auth\LoginAction;
+use App\Actions\Auth\RegisterAction;
+use Illuminate\Support\Facades\Route;
+use Inertia\Inertia;
+
+Route::prefix('/login')->name('login.')->group(function () {
+
+    Route::get('/',
+        function () {
+            return Inertia::render('Auth/Login');
+        })
+        ->name('create');
+
+    Route::post('/login', LoginAction::class);
+
+//    Route::delete('/', 'destroy')->name('destroy');
+});
+
+Route::prefix('/register')->name('register.')->group(function () {
+    Route::get('/',
+        function () {
+            return Inertia::render('Auth/Register');
+        })
+        ->name('create');
+
+    Route::post('/', RegisterAction::class)->name('store');
+});
--- routes/web.php
+++ routes/web.php
@@ -1,30 +1,12 @@
 <?php
 
-use App\Actions\Auth\LoginAction;
-use App\Actions\Auth\RegisterAction;
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
 
 Route::get('/', function () {
 
-    return \Inertia\Inertia::render('Home');
+    return Inertia::render('Home');
 });
 
+require 'auth.php';
 
-Route::prefix('/login')->name('login.')->group(function (){
-
-    Route::get('/', function (){
-        return Inertia::render('Auth/Login');
-    })->name('create');
-
-    Route::post('/login', LoginAction::class);
-
-//    Route::delete('/', 'destroy')->name('destroy');
-});
-
-Route::prefix('/register')->name('register.')->group(function (){
-    Route::get('/', function (){
-        return Inertia::render('Auth/Register');
-    })->name('create');
-    Route::post('/', RegisterAction::class)->name('store');
-});
