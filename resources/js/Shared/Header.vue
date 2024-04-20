19401972490d733faf446228ccc767754fd30aca
Huda Shakir <huda.f.shakir@gmail.com>
2024-04-20 18:01:36 +0300

    deploy: resized down images for smaller bundle size

--- resources/js/Shared/Header.vue
+++ resources/js/Shared/Header.vue
@@ -8,7 +8,7 @@ let username = usePage().props.auth.user.name;
 <template>
     <header class="bg-white border-gray-200">
         <div class="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
-            <img src="../../images/logo.png" class="h-16" alt="EverBite Logo"/>
+            <img src="../../images/logo.webp" class="h-16" alt="EverBite Logo"/>
             <div v-if="!username" class="flex md:order-2 space-x-3 md:space-x-0 rtl:space-x-reverse">
                 <Link href="/register" as="button"
                         class="text-white bg-yellow-500 hover:bg-yellow-600 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 text-center me-3">
